1e19b3e48b9c6a605a42acc52f918636d6f69de9
jaine <essi@shipyard.games>
2018-12-21 21:24:26 +0200

    Initial Commit

---
+++ omatpysakit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  omatpysakit
+//
+//  Created by Essi Jukkala on 21/12/2018.
+//  Copyright © 2018 Essi Jukkala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
